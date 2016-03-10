b238e83cf050066a939f77f4cab57ff605b6536d
João Mota <Joo Mota>
2016-03-10 16:54:33 +0000

    tp3 almost there

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,104 +1,55 @@
 package Client;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.net.Socket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
 public class Client {
 	public static void main(String[] args) {
 		
-//		for(String str: args){
-//			System.out.println(str);
-//		}
-		
-		DatagramSocket dSock = null;
-		MulticastSocket mSock = null;
-
-		try {
-			mSock = new MulticastSocket(Integer.parseInt(args[1]));
-		} catch (SocketException e) {
-			System.out.println("CLIENT: Could not create socket, exiting now");
-			return;
-		} catch (NumberFormatException e) {
-			System.out.println("CLIENT: Port argument does not apear to be a number");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		for(String str: args){
+			System.out.println(str);
 		}
-
-		byte[] mbuf = new byte[256];
-		DatagramPacket mPack = new DatagramPacket(mbuf, mbuf.length);
-		DatagramPacket dPack = null;
+		
+		
+		Socket cSock = null;
 		try {
-			mSock.joinGroup(InetAddress.getByName(args[0]));
+			cSock = new Socket(InetAddress.getByName(args[0]), Integer.parseInt(args[1]));
+			BufferedReader cBufRead = new BufferedReader(new InputStreamReader(cSock.getInputStream()));
+			PrintWriter cPrntWriter = new PrintWriter(cSock.getOutputStream());
 			
 			String message = args[2];
 			for(int i = 3; i < args.length; i++){
 				message += (" " + args[i]);
 			}
 			
-			dPack = new DatagramPacket(message.getBytes(), message.length());
-		} catch (UnknownHostException e) {
-			System.out.println("CLIENT: Could not connect to server, exiting now");
-			return;
-		} catch (IOException e) {
-			System.out.println("CLIENT: Could not send datagram, exiting now");
-		}
-		
-		try {
-			mSock.receive(mPack);
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-		String mReceived = null;
-		try {
-			mReceived = new String(mbuf, "UTF-8").trim();
-		} catch (UnsupportedEncodingException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		System.out.println(mReceived);
-		String[] serverData = mReceived.split(" ");
-		
-		try {
-			dSock = new DatagramSocket(8888);
-			dSock.connect(InetAddress.getByName(serverData[0]), Integer.parseInt(serverData[1]));
+			System.out.println("Gonna send: " + message);
+			cPrntWriter.println(message);
+			cPrntWriter.flush();
 			
-			dSock.send(dPack);
-		} catch (SocketException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (NumberFormatException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-		
-		
-		byte[] buf = new byte[256];
-		DatagramPacket dPackResp = new DatagramPacket(buf, buf.length);
-		try {
-			dSock.receive(dPackResp);
-			String resp = new String(buf, "UTF-8").trim();
-			System.out.println(resp);
-		} catch (IOException e) {
+			while(!cBufRead.ready()){
+				;
+			}
+			
+			String response = cBufRead.readLine();
+			
+			System.out.println(response);
+			
+			cSock.shutdownOutput();
+			
+		} catch (NumberFormatException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-	}
+	}	
 }
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -3,7 +3,10 @@ import java.util.Hashtable;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.regex.Pattern;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.*;
 
@@ -18,172 +21,150 @@ public class Server {
 			System.out.println(str);
 		}*/
 		
-		DatagramSocket dSock = null;
-		MulticastSocket mSock = null;
+		ServerSocket servSock = null;
 		try {
-			dSock = new DatagramSocket(Integer.parseInt(args[0]));
-			dSock.setSoTimeout(1000);
-			
-			mSock = new MulticastSocket(Integer.parseInt(args[2]));
-			mSock.joinGroup(InetAddress.getByName(args[1]));
-		} catch (SocketException e) {
-			System.out.println("SERVER: Could not create sockets, exiting now.");
-			return;
-		}
-		catch(NumberFormatException e){
-			System.out.println("SERVER: Argument for port number is not a number");
-		} catch (IOException e) {
+			servSock = new ServerSocket(Integer.parseInt(args[0]));
+		} catch (NumberFormatException | IOException e1) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e1.printStackTrace();
 		}
-		
-		
-		
-		String broadcastMsg = null;
-		try {
-			broadcastMsg = InetAddress.getLocalHost().getHostAddress();
-		} catch (UnknownHostException e2) {
-			// TODO Auto-generated catch block
-			e2.printStackTrace();
-		}
-		broadcastMsg += (" " + args[0]);
-		
-		
-		
+		Socket sSock = null;
+	
 		System.out.println("Server is running");
 		while(true){
-			DatagramPacket mPack;
 			try {
-				mPack = new DatagramPacket(broadcastMsg.getBytes(), broadcastMsg.length(), InetAddress.getByName(args[1]), Integer.parseInt(args[2]));
-				mSock.send(mPack);
-			} catch (NumberFormatException e2) {
+				
+				sSock = servSock.accept();
+				
+				Thread sThread = new Thread(new ServerThread(sSock));
+				sThread.start();
+			} catch (NumberFormatException | IOException e) {
 				// TODO Auto-generated catch block
-				e2.printStackTrace();
-			} catch (UnknownHostException e2) {
-				// TODO Auto-generated catch block
-				e2.printStackTrace();
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	
+	public static class ServerThread implements Runnable{
+		Socket sock;
+		BufferedReader sBufRead;
+		PrintWriter sPrntWriter;
+		byte[] msg;
+		
+		public ServerThread(Socket sock){
+			System.out.println("New thread running");
+			this.sock = sock;
+		}
+		
+		
+		@Override
+		public void run() {
+			try {
+				sBufRead = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+				sPrntWriter = new PrintWriter(sock.getOutputStream());
+				
+				String received = null;
+				
+				
+				
+				if(sBufRead.ready()){
+					received = sBufRead.readLine();
+				}
+				System.out.println("Received: " + received);
+				
+				this.handleReceived(received, sPrntWriter);
+				
+				
+			} catch (IOException e) {
+				System.err.println("SERVER: Could not establish I/O channels, exiting now.");
+				System.exit(-1);
+			}
+		}
+		
+		public int register(String plate, String owner){
+			if(db.containsKey(plate)){
+				return -1;
+			}
+			db.put(plate, owner);
+			return db.size();
+		}
+		
+		public String lookup(String plate){
+			if(db.containsKey(plate)){
+				return db.get(plate);
+			}
+			return "NOT_FOUND";
+		}
+		
+		public void handleReceived(String received, PrintWriter writer){
+			if(received == null){
+				return;
+			}
+			
+			System.out.println(received);
+			
+			String[] rec2 = received.split(" ");
+			boolean goodCommand = true;
+			
+			switch(rec2[0]){
+				case "reg":
+					if(rec2.length != 3){
+						System.out.println("SERVER: Register request with wrong number of arguments");
+						goodCommand = false;
+					}
+					if(goodCommand && (!checkValidPlate(rec2[1]) || rec2[2].length() > 256)){
+						System.out.println("SERVER: Plate is not in the right format or owner name is too big");
+						goodCommand = false;
+					}
+					
+					if(goodCommand){
+						int resp = register(rec2[1], rec2[2]);
+						writer.println(Integer.toString(resp));
+					}
+					else{
+						writer.println(Integer.toString(-1));
+					}
+					writer.flush();
+					
+					break;
+					
+				case "look":
+					if(rec2.length != 2){
+						System.out.println("SERVER: Lookup request with wrong number of arguments");
+						goodCommand = false;
+					}
+					if(rec2[1].length() > 256){
+						System.out.println("SERVER: Owner name is too big");
+					}
+					
+					if(goodCommand){
+						String owner = lookup(rec2[1]);
+						writer.println(owner);
+					}
+					else{
+						writer.println("ERROR_NOT_CORRECT_COMMAND");
+					}
+					writer.flush();
+				default:
+					System.out.println("SERVER: Unknown request");
+					break;
+			}
+			
+			try {
+				sock.shutdownInput();
+				sock.close();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
-			
-			DatagramPacket dPack = null;
-			byte[] buf = new byte[275];
-			
-			try {
-				dPack = new DatagramPacket(buf, buf.length);
-				dSock.receive(dPack);
-			}catch (SocketTimeoutException e){
-				System.out.println("SERVER: Receive timed out, restarting loop");
-				continue;
-			}
-			catch (IOException e) {
-				System.out.println("SERVER: Something went wrong while receiving messages");
-				return;
-			}
-			
-			
-			String dataReceived = null;
-			
-			try {
-				dataReceived = new String(buf, "UTF-8").trim();
-			} catch (UnsupportedEncodingException e) {
-				System.out.println("SERVER: Data received was not text, resuming operation.");
-			}
-			
-			if(dataReceived.equals(""));
-			
-			handleReceived(dataReceived, dSock, dPack);
-		}
-	}
-	
-	public static int register(String plate, String owner){
-		if(db.containsKey(plate)){
-			return -1;
-		}
-		db.put(plate, owner);
-		return db.size();
-	}
-	
-	public static String lookup(String plate){
-		if(db.containsKey(plate)){
-			return db.get(plate);
-		}
-		return "NOT_FOUND";
-	}
-	
-	public static void handleReceived(String received, DatagramSocket dSock, DatagramPacket dPack){
-		if(received == null){
-			return;
 		}
 		
-		String[] rec2 = received.split(" ");
-		boolean goodCommand = true;
+		public boolean checkValidPlate(String plate){
+			return Pattern.matches("([0-9A-Z]{2}-){2}[0-9A-Z]{2}", plate);
+		}
 		
-		switch(rec2[0]){
-			case "reg":
-				if(rec2.length != 3){
-					System.out.println("SERVER: Register request with wrong number of arguments");
-					goodCommand = false;
-				}
-				if(goodCommand && (!checkValidPlate(rec2[1]) || rec2[2].length() > 256)){
-					System.out.println("SERVER: Plate is not in the right format or owner name is too big");
-					goodCommand = false;
-				}
-				
-				DatagramPacket dPackResp = null;
-				if(goodCommand){
-					int resp = register(rec2[1], rec2[2]);
-					
-					dPackResp = new DatagramPacket(new Integer(resp).toString().getBytes(), new Integer(resp).toString().length(),
-							dPack.getAddress(), dPack.getPort());
-				}
-				else{
-					dPackResp = new DatagramPacket("-1".getBytes(), 2, dPack.getAddress(), dPack.getPort());
-				}
-				
-				try {
-					dSock.send(dPackResp);
-				} catch (IOException e) {
-					System.out.println("SERVER: Could not send response to client, resuming operation");
-				}
-				
-				break;
-			case "look":
-				if(rec2.length != 2){
-					System.out.println("SERVER: Lookup request with wrong number of arguments");
-					goodCommand = false;
-				}
-				if(rec2[1].length() > 256){
-					System.out.println("SERVER: Owner name is too big");
-				}
-				
-				if(goodCommand){
-					String owner = lookup(rec2[1]);
-					
-					dPackResp = new DatagramPacket(owner.getBytes(), owner.length(),
-							dPack.getAddress(), dPack.getPort());
-				}
-				else{
-					dPackResp = new DatagramPacket("ERROR_NOT_CORRECT_COMMAND".getBytes(), "ERROR_NOT_CORRECT_COMMAND".length(), dPack.getAddress(), dPack.getPort());
-				}
-				
-				try {
-					dSock.send(dPackResp);
-				} catch (IOException e) {
-					System.out.println("SERVER: Could not send response to client, resuming operation");
-				}
-				
-				
-			default:
-				System.out.println("SERVER: Unknown request");
-				break;
-		}	
-	}
-	
-	public static boolean checkValidPlate(String plate){
-		return Pattern.matches("([0-9A-Z]{2}-){2}[0-9A-Z]{2}", plate);
 	}
 }
 	
